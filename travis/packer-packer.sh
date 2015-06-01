74065d87bd9b71b48cbe8916ad2241c2e56990b3
Pedro Barbosa <jpedro.barbosa@gmail.com>
2015-06-01 22:45:53 +0200

    Fix wrong file name

--- travis/packer-packer.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -eu
-
-cd packer
-wget https://dl.bintray.com/mitchellh/packer/packer_0.7.5_linux_amd64.zip
-unzip -o packer_0.7.5_linux_amd64.zip
-sudo mv packer* /usr/bin/
